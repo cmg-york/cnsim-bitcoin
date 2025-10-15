a0f3f5f357f69ec64019ec0212287b41af9041d5
liaskos <liaskos@yorku.ca>
2025-10-14 23:59:06 -0400

    fix(BitcoinNode)-minor: fixing parameters in beliefReport

--- src/main/java/ca/yorku/cmg/cnsim/bitcoin/node/BitcoinNode.java
+++ src/main/java/ca/yorku/cmg/cnsim/bitcoin/node/BitcoinNode.java
@@ -137,7 +137,11 @@ public class BitcoinNode extends Node {
 	public void beliefReport(long[] sample, long time) {
 		if (Reporter.reportsBeliefs() || Reporter.reportsBeliefsShort()) 
 			for (int i = 0; i < sample.length; i++) {
-				Reporter.addBeliefEntry(this.sim.getSimID(), this.getID(), sample[i], blockchain.transactionBelieved(sample[i]), time);	
+				Reporter.addBeliefEntry(
+						this.sim.getSimID(), this.getID(), sample[i], 
+						blockchain.transactionBelieved(sample[i]),
+						blockchain.transactionBelief(sample[i]),
+						time, false);
 			}
 	}
 
